033b667d350f2040db36af08962d7babccbe8fb1
zori13 <zori.gon@mail.com>
2023-11-26 17:41:31 +0100

    Ejercicio 4 / Contador

---
+++ src/components/Ejercicio4/Ejercicio4.jsx
@@ -0,0 +1,10 @@
+import Contador from './Contador'
+import TituloEjercicio from '../TituloEjercicio'
+export default function Ejercicio4(){
+    return(
+        <section className="caja-ejercicio">
+            <TituloEjercicio>Ejercicio 4</TituloEjercicio>   
+        <Contador/>
+        </section>    
+    )
+}
