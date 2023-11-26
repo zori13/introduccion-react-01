a1094dd025c13d05cfcf7459507e97642131d917
zori13 <zori.gon@mail.com>
2023-11-26 17:39:00 +0100

    Ejercicio 3 / Lista condicional

--- src/components/Ejercicio3/Ejercicio3.jsx
+++ src/components/Ejercicio3/Ejercicio3.jsx
@@ -4,7 +4,7 @@ import { productosCondicional } from "../../constants/productos"
 export default function Ejercicio3() {
     return (
         <section className="caja-ejercicio">
-            <TituloEjercicio>Ejercicio 3</TituloEjercicio>
+            <TituloEjercicio> Ejercicio 3 </TituloEjercicio>
             <ListaProductosCondicional productos={productosCondicional}/>
         </section>
     )
--- src/components/Ejercicio3/ListaProductosCondicional.jsx
+++ src/components/Ejercicio3/ListaProductosCondicional.jsx
@@ -2,7 +2,7 @@ export default function ListaProductos(props){
     return(
         <div>
             <h2>Productos</h2>
-            {props.productos.length===0?(
+            {props.productos.length === 0?(
             <p>La lista está vacía</p>) :(
             <ol>
                 {props.productos.map((producto,index)=>(
